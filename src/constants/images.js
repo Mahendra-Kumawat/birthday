0b3d2c09ec093e4f0ad42cad1165f3c28501524a
Mahemdra-Kumawat <maheekumawat9@gmail.com>
2026-05-12 21:56:43 +0530

    remove unnecessary images

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,12 @@ import memory01 from "../assets/memory_01.jpeg";
 import memory02 from "../assets/memory_02.jpeg";
 import memory05 from "../assets/memory_05.jpeg";
 import memory06 from "../assets/memory_06.jpeg";
-import memory07 from "../assets/memory_07.jpeg";
+// import memory07 from "../assets/memory_07.jpeg";
 import memory08 from "../assets/memory_08.jpeg";
 import memory09 from "../assets/memory_09.jpeg";
-import memory10 from "../assets/memory_10.jpeg";
-import memory11 from "../assets/memory_11.jpeg";
-import memory12 from "../assets/memory_12.jpeg";
+// import memory10 from "../assets/memory_10.jpeg";
+// import memory11 from "../assets/memory_11.jpeg";
+// import memory12 from "../assets/memory_12.jpeg";
 import memory13 from "../assets/memory_13.jpeg";
 import memory14 from "../assets/memory_14.jpeg";
 import memory15 from "../assets/memory_15.jpeg";
@@ -17,7 +17,7 @@ import memory18 from "../assets/memory_18.jpeg";
 import memory19 from "../assets/memory_19.jpeg";
 import memory20 from "../assets/memory_20.jpeg";
 import memory21 from "../assets/memory_21.jpeg";
-import memory22 from "../assets/memory_22.jpeg";
+// import memory22 from "../assets/memory_22.jpeg";
 import memory23 from "../assets/memory_23.jpeg";
 import memory24 from "../assets/memory_24.jpeg";
 import memory25 from "../assets/memory_25.jpeg";
@@ -37,7 +37,7 @@ import memory38 from "../assets/memory_38.jpeg";
 import memory39 from "../assets/memory_39.jpeg";
 import memory40 from "../assets/memory_40.jpeg";
 import memory41 from "../assets/memory_41.jpeg";
-import memory42 from "../assets/memory_42.jpeg";
+// import memory42 from "../assets/memory_42.jpeg";
 import memory43 from "../assets/memory_43.jpeg";
 import memory44 from "../assets/memory_44.jpeg";
 import memory45 from "../assets/memory_45.jpeg";
@@ -49,12 +49,8 @@ export const allImages = [
   { id: 2, img: memory02 },
   { id: 5, img: memory05 },
   { id: 6, img: memory06 },
-  { id: 7, img: memory07 },
   { id: 8, img: memory08 },
   { id: 9, img: memory09 },
-  { id: 10, img: memory10 },
-  { id: 11, img: memory11 },
-  { id: 12, img: memory12 },
   { id: 13, img: memory13 },
   { id: 14, img: memory14 },
   { id: 15, img: memory15 },
@@ -64,7 +60,6 @@ export const allImages = [
   { id: 19, img: memory19 },
   { id: 20, img: memory20 },
   { id: 21, img: memory21 },
-  { id: 22, img: memory22 },
   { id: 23, img: memory23 },
   { id: 24, img: memory24 },
   { id: 25, img: memory25 },
@@ -84,7 +79,6 @@ export const allImages = [
   { id: 39, img: memory39 },
   { id: 40, img: memory40 },
   { id: 41, img: memory41 },
-  { id: 42, img: memory42 },
   { id: 43, img: memory43 },
   { id: 44, img: memory44 },
   { id: 45, img: memory45 },
